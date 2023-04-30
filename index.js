cd1faa6539d1519efd87c9915add9197bc39f8e9
William A. Ferguson <wilsb8@gmail.com>
2023-04-30 10:58:34 +0000

    template setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http"); // require http
+const app = require("./app/app");
+// require(ment) declaration
+require("dotenv").config(); // read from our .env file
+
+
+// create listen server
+http.createServer(app).listen(process.env.PORT || 5000, ()=> {
+    console.log(`Server here. I am fully operational and listening on port: ${process.env.PORT}`)
+});
---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const reqPath = path.join(__dirname); 
+
+router.get('/', (req, res) => {
+    console.log(reqPath);
+    res.sendFile(path.join(reqPath, 'views/index.html'));
+});
+
+
+
+module.exports = router;
