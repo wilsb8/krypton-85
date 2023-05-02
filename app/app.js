4f29d953e510c29f9ee6ee0ca0c332349316a538
William A. Ferguson <wilsb8@gmail.com>
2023-05-02 08:49:25 +0000

    php

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const sphp = require('sphp');
 const route  = require('../routes/routes');
 const app = express();
 // serve static content
 app.use(express.static('public'));
+// for php
+app.use(sphp.express('public/'));
+// 
 app.use(express.static('views'));
 app.use('/', route);
 module.exports = app; 
--- public/forms/contact.php
+++ public/forms/contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'seamsobvious@gmail.com';
 
-  if( file_exists($php_email_form = 'public/vendor/php-email-form' )) {
+  if( file_exists($php_email_form = 'vendor/php-email-form' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
