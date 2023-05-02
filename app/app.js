a510b3dd534fa9da75319eb29366a834e85dcfe4
William A. Ferguson <wilsb8@gmail.com>
2023-05-02 11:11:39 +0000

    php

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const sphp = require('sphp');
 const route  = require('../routes/routes');
 const app = express();
 // serve static content
-app.use(sphp.express('public'));
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use('/', route);
