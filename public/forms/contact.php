00b23c022f3ba6269957d3c84f25407b27694d0a
William A. Ferguson <wilsb8@gmail.com>
2023-05-02 10:13:52 +0000

    php

--- public/forms/contact.php
+++ public/forms/contact.php
@@ -18,18 +18,18 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = false;
   
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
 
-  $contact->smtp = array(
-    'host' => 'gmail.com',
-    'username' => 'seamsobvious@gmail.com',
-    'password' => 'Csewalt2023',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'gmail.com',
+  //   'username' => 'seamsobvious@gmail.com',
+  //   'password' => 'Csewalt2023',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
