c1484558e8fa87bf3bb62cfdbed0cf8f4eaa067e
William A. Ferguson <wilsb8@gmail.com>
2023-05-02 10:10:04 +0000

    php

--- public/forms/contact.php
+++ public/forms/contact.php
@@ -18,20 +18,19 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'gmail.com',
+    'username' => 'seamsobvious@gmail.com',
+    'password' => 'Csewalt2023',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
