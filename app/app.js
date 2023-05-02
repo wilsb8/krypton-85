b439c5794b6543270e081dc09eab5b2b6c06cbaf
wilsb8 <wilsb8@gmail.com>
2023-05-02 12:00:24 -0700

    started nodemailer

--- app/app.js
+++ app/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const route  = require('../routes/routes');
 const app = express();
+// static directories
 app.use(express.static('public'));
 app.use(express.static('views'));
+
+// enable cors
+const cors = require("cors");
+app.use(cors);
+
+
+
+// for accepting post from data
+const bodyParser = require("express");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.use('/', route);
 
 module.exports = app; 
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const nodemailer = require('nodemailer');
 const reqPath = path.join(__dirname); 
+
 
 router.get('/', (req, res) => {
     console.log(reqPath);
