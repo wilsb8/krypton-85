db0ea51b566a2cd7147d8e7ecc22e62a1bd24db8
William A. Ferguson <wilsb8@gmail.com>
2023-05-02 13:18:52 +0000

    changing contact emailer to nodemail

--- app/app.js
+++ app/app.js
@@ -1,12 +1,8 @@
 const express = require('express');
-// const sphp = require('sphp');
 const route  = require('../routes/routes');
 const app = express();
-
-// serve static content
-//app.use(sphp.express('public'));
-
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use('/', route);
+
 module.exports = app; 
