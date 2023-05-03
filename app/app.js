8d8ed9ced45f42cd98cd690a4b90715101e19cdf
William A. Ferguson <wilsb8@gmail.com>
2023-05-03 09:24:00 +0000

    change to app

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const route  = require('../routes/routes');
 const app = express();
+
 // static directories
 app.use(express.static('public'));
 app.use(express.static('views'));
@@ -16,8 +17,51 @@ const bodyParser = require("express");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// send route
+app.post('/send', (req, res) => {
+    const output = `
+      <p>You have a new contact request</p>
+      <h3>Contact Details</h3>
+      <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Subject: ${req.body.subject}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+  
+    // create reusable transporter object using the default SMTP transport
+    let mailTransporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'seamsobvious@gmail.com',
+          pass: 'Csewalt2023'
+      }
+  });
+  
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <seamsobvious@gmail.com>', // sender address
+        to: 'Seams Obvious', 
+        subject: 'Node Contact Request', // Subject line
+        text: 'You have a message from the website!', // plain text body
+        html: output // html body
+    };
+  
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.json({ status: 'error-message', message: 'There was a problem sending your message. Please try again later.' });
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.json({ status: 'sent-message', message: 'Your message has been sent. Thank you!' });
+      }
+    });
+});
 
 app.use('/', route);
-app.use('/send', route)
+
 
 module.exports = app; 
--- routes/routes.js
+++ routes/routes.js
@@ -10,48 +10,7 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(reqPath, 'views/index.html'));
 });
 
-router.post('/send', (req, res) => {
-    const output = `
-      <p>You have a new contact request</p>
-      <h3>Contact Details</h3>
-      <ul>  
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-        <li>Subject: ${req.body.subject}</li>
-      </ul>
-      <h3>Message</h3>
-      <p>${req.body.message}</p>
-    `;
-  
-    // create reusable transporter object using the default SMTP transport
-    let mailTransporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-          user: 'seamsobvious@gmail.com',
-          pass: 'Csewalt2023'
-      }
-  });
-  
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Nodemailer Contact" <seamsobvios@gmail.com>', // sender address
-        to: 'RECEIVEREMAILS', // list of receivers
-        subject: 'Node Contact Request', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output // html body
-    };
-  
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.json({ status: 'error-message', message: 'There was a problem sending your message. Please try again later.' });
-      } else {
-        console.log('Email sent: ' + info.response);
-        res.json({ status: 'sent-message', message: 'Your message has been sent. Thank you!' });
-      }
-    });
-  });
+
 
 
 
