2a83f6d88c9b2ef93c4fcae209388f48db356fb1
William A. Ferguson <wilsb8@gmail.com>
2023-05-03 12:33:12 +0000

    update

--- app/app.js
+++ app/app.js
@@ -53,10 +53,10 @@ app.post('/send', (req, res) => {
     mailTransporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
-        res.json({ status: 'error-message', message: 'There was a problem sending your message. Please try again later.' });
+        res.render({ status: 'error-message', message: 'There was a problem sending your message. Please try again later.' });
       } else {
         console.log('Email sent: ' + info.response);
-        res.json({ status: 'sent-message', message: 'Your message has been sent. Thank you!' });
+        res.render({ status: 'sent-message', message: 'Your message has been sent. Thank you!' });
         }
     });
 });
