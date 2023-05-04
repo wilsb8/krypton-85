592d4100eff2b4eb0e8a0bb3e4a73a5d380ac8ae
wilsb8 <wilsb8@gmail.com>
2023-05-04 15:43:13 -0700

    completed

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ app.post('/send', (req, res) => {
   let mailOptions = {
     from: '"Nodemailer Contact Form" <no-reply@seamsovious.com>', // sender address
     to: 'seamsobvious@gmail.com',
-    subject: 'Node Contact Request', // Subject line
+    subject: 'Seams Obvious Contact Request', // Subject line
     text: 'You have a message from the website!', // plain text body
     html: output // html body
   };
