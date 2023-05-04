d6b65e2f08aafb3a980135d0c3d3bbb92048ced3
wilsb8 <wilsb8@gmail.com>
2023-05-04 09:04:57 -0700

    nodemailer tested and working

--- app/app.js
+++ app/app.js
@@ -15,51 +15,57 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // post route
 app.post('/send', (req, res) => {
-    console.log(req.body);
-    const output = `
-      <h2>You have a new contact request</h2>
-      <h3>Contact Details</h3>
-      <ul>  
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-        <li>Subject: ${req.body.subject}</li>
-      </ul>
-      <h3>Message</h3>
-      <p>${req.body.message}</p>
-    `;
-  
-    // create reusable transporter object using the default SMTP transport
-    var mailTransporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: "e6d2eb0325ba50",
-        pass: "191168b8a27666"
-      }
-    });
-  
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Nodemailer Contact Form" <no-reply@seamsovious.com>', // sender address
-        to: 'seamsobvious@gmail.com', 
-        subject: 'Node Contact Request', // Subject line
-        text: 'You have a message from the website!', // plain text body
-        html: output // html body
-    };
-  
-    // send mail with defined transport object
-    mailTransporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        res.render('There was a problem sending your message. Please try again later.');
-        console.log(error)
-      } else {
-        console.log('Email sent: ' + info.response);
-        console.log('Message: ' + output)
-        //res.json({ status: 'sent-message', message: 'Your message has been sent. Thank you!' });
-        res.render('/')
-        }
-    });
+  console.log(req.body);
+  const output = `
+    <h2>You have a new contact request</h2>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Subject: ${req.body.subject}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  var mailTransporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "e6d2eb0325ba50",
+      pass: "191168b8a27666"
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Nodemailer Contact Form" <no-reply@seamsovious.com>', // sender address
+    to: 'seamsobvious@gmail.com',
+    subject: 'Node Contact Request', // Subject line
+    text: 'You have a message from the website!', // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  mailTransporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({ status: 'ERROR', message: 'There was a problem sending your email!' });
+      console.log(error);
+      setTimeout(() => {
+        res.redirect('/');
+      }, 3000);
+    } else {
+      console.log('Email sent: ' + info.response);
+      console.log('Message: ' + output)
+      console.log('Mail sent!')
+      setTimeout(() => {
+        res.redirect('/');
+      }, 3000);
+    }
+  });
 });
+
 
 app.use('/', route);
 
