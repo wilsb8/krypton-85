8103a5077cf84b0d0ceac0dd8cf8faee3a99feba
wilsb8 <wilsb8@gmail.com>
2023-05-11 08:03:46 -0700

    experimenting with status messages

--- app/app.js
+++ app/app.js
@@ -79,7 +79,6 @@ app.post('/send', (req, res) => {
       console.log('Message: ' + output)
       console.log('Mail sent!')
       setTimeout(() => {
-        alert("Your message has been sent. Thank you.")
         res.redirect('/');
       }, 2000);
     }
